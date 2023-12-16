027f6ee2468284d95ed3d8ce91754bebf0b4deee
katielock92 <katielock92@gmail.com>
2023-12-16 14:32:10 +1100

    feat: testing homepage routes for netlify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 // TODO
 // This will be the page component to hold all other components specific to the Home page
 
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
     return(
+        <div>
         <h1>Homepage Placeholder</h1>
+        <p>Let's test these routes...</p>
+        <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/profile/6575733ec77e4fcca14b175d">Profile for Ed</Link></li>
+            <li><Link to="/reports">Reports</Link></li>
+        </ul>
+        </div>
     )
 }
