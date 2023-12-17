c010cc6fd35abd91c936c12327a4a60fd0a1528b
katielock92 <katielock92@gmail.com>
2023-12-17 21:09:53 +1100

    feat: login route WIP
    - added basic front-end validation for login (email and password required fields)
    - added alert popups for unsuccessful logins

--- src/components/Login/SignIn.jsx
+++ src/components/Login/SignIn.jsx
@@ -7,6 +7,7 @@ import {
   Checkbox,
   Box,
   Container,
+  Alert,
 } from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 
@@ -16,6 +17,8 @@ import { useState } from "react";
 import { loginUrl } from "../../utils/ApiPaths";
 
 const SignIn = ({ setView }) => {
+  // Establishing states
+  const [errorMessage, setErrorMessage] = useState("");
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -53,7 +56,25 @@ const SignIn = ({ setView }) => {
     })
       .then((response) => {
         if (!response.ok) {
-          // Add front-end validation when this occurs
+          if (response.status === 400) {
+            setErrorMessage(
+              <Alert severity="error">
+                Incorrect email or password, please try again.
+              </Alert>
+            );
+          } else {
+            setErrorMessage(
+              <Alert severity="error">
+                Uh oh, we couldn't log you in! Please try again.
+              </Alert>
+            );
+          }
+
+          // Clear the error message after 5 seconds
+          setTimeout(() => {
+            setErrorMessage("");
+          }, 5000);
+          
           throw new Error("Login failed");
         }
         return response.json();
@@ -83,82 +104,86 @@ const SignIn = ({ setView }) => {
   // TODO: take inline styling into css file where possible
 
   return (
-    <Container>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <LoginLogo />
+    <div>
+      {/* Display any error message here */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+      <Container>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <LoginLogo />
 
-        <LoginText />
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                value={formData.rememberMe}
-                color="primary"
-                name="rememberMe"
-                onChange={handleChange}
-              />
-            }
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            endIcon={<ArrowForward />}
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Log in
-          </Button>
-          <Box className="buttonBox">
-            <div>
-              <Button size="medium" onClick={handleForgotPasswordClick}>
-                Forgot password?
-              </Button>
-            </div>
-            <div>
-              <Button
-                size="medium"
-                onClick={handleGoBackClick}
-                startIcon={<ArrowBack />}
-              >
-                Go back
-              </Button>
-            </div>
+          <LoginText />
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  value={formData.rememberMe}
+                  color="primary"
+                  name="rememberMe"
+                  onChange={handleChange}
+                />
+              }
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              endIcon={<ArrowForward />}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log in
+            </Button>
+            <Box className="buttonBox">
+              <div>
+                <Button size="medium" onClick={handleForgotPasswordClick}>
+                  Forgot password?
+                </Button>
+              </div>
+              <div>
+                <Button
+                  size="medium"
+                  onClick={handleGoBackClick}
+                  startIcon={<ArrowBack />}
+                >
+                  Go back
+                </Button>
+              </div>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
