8684b94532044eca4f4c84ad6cdc7df726af9f4e
katielock92 <katielock92@gmail.com>
2023-12-19 15:06:40 +1100

    feat: progress on add user feature
    - line manager drop-down list populated with users who have line manager ticked against them
    - this adds their user id into the request when adding user
    - added basic front-end error for when there is a back-end error
    - still need to add regex validation for email in front-end

--- src/components/Settings/AddUser.jsx
+++ src/components/Settings/AddUser.jsx
@@ -3,13 +3,15 @@ import {
   Alert,
   Button,
   Checkbox,
+  CircularProgress,
   FormControlLabel,
   MenuItem,
   TextField,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import userRoles from "../../utils/UserRoles";
 import { apiUrl } from "../../utils/ApiUrl";
+import axios from "axios";
 
 export default function AddUser(props) {
   // Set default values of formData
@@ -24,7 +26,40 @@ export default function AddUser(props) {
   });
 
   // Establishing states
+  const [fullUsers, setFullUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [successMessage, setSuccessMessage] = useState("");
+
+  // Importing full users list to render in form
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Retrieve token from local storage
+        const jwtToken = localStorage.getItem("jwtToken");
+
+        // Include the token in the GET request header
+        const response = await axios.get(apiUrl + "users/all/fullusers", {
+          headers: {
+            Authorization: `Bearer ${jwtToken}`,
+          },
+        });
+
+        const sortedUsers = response.data.Users.sort((a, b) =>
+          `${a.name.first} ${a.name.last}`.localeCompare(
+            `${b.name.first} ${b.name.last}`
+          )
+        );
+
+        setFullUsers(sortedUsers);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleClick = () => {
     props.toggle();
@@ -68,8 +103,7 @@ export default function AddUser(props) {
       isFullUser: true,
       isAdmin: isAdmin,
       isSeniorManager: isSeniorManager,
-      //   TODO: add this later once form is rendering list
-      userLineManager: "",
+      userLineManager: formData.userLineManager,
     });
 
     const jwtToken = localStorage.getItem("jwtToken");
@@ -86,13 +120,14 @@ export default function AddUser(props) {
     })
       .then((response) => {
         if (!response.ok) {
-          // TODO: front-end validation here
           throw new Error("Request failed");
         }
         return response.json();
       })
       .catch((error) => {
-        // TODO: Add front-end validation when this occurs
+        setSuccessMessage(
+            <Alert severity="error">Uh oh, we're having a little difficulty here! Please try again.</Alert>
+          );
         console.error("Error:", error);
       });
 
@@ -136,130 +171,152 @@ export default function AddUser(props) {
 
   return (
     <div className="modal">
-      <div className="modal_content">
-        <span className="close" onClick={handleClick}>
-          &times;
-        </span>
-        <h2 className="formHeading">Add user</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="cardForm">
-            <div className="formRow">
-              <div className="formSelect">
-                <TextField
-                  required
-                  id="firstName"
-                  name="firstName"
-                  variant="outlined"
-                  label="First name"
-                  className="formSelector"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                />
+      {loading ? (
+        <div className="loader">
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className="modal_content">
+          <span className="close" onClick={handleClick}>
+            &times;
+          </span>
+          <h2 className="formHeading">Add user</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="cardForm">
+              <div className="formRow">
+                <div className="formSelect">
+                  <TextField
+                    required
+                    id="firstName"
+                    name="firstName"
+                    variant="outlined"
+                    label="First name"
+                    className="formSelector"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="formSelect">
+                  <TextField
+                    required
+                    id="lastName"
+                    name="lastName"
+                    variant="outlined"
+                    label="Last name"
+                    className="formSelector"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-              <div className="formSelect">
-                <TextField
-                  required
-                  id="lastName"
-                  name="lastName"
-                  variant="outlined"
-                  label="Last name"
-                  className="formSelector"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
 
-            <div className="formRow">
-              <div className="formSelect">
-                {/* add front-end validation on email regex */}
-                <TextField
-                  required
-                  id="email"
-                  name="email"
-                  variant="outlined"
-                  label="Email"
-                  className="formSelector"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
+              <div className="formRow">
+                <div className="formSelect">
+                  {/* add front-end validation on email regex */}
+                  <TextField
+                    required
+                    id="email"
+                    name="email"
+                    variant="outlined"
+                    label="Email"
+                    className="formSelector"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="formRow">
-              <div className="formSelect">
-                <TextField
-                  required
-                  id="businessUnit"
-                  name="businessUnit"
-                  variant="outlined"
-                  className="formSelector"
-                  label="Business unit"
-                  value={formData.businessUnit}
-                  onChange={handleChange}
-                />
+              <div className="formRow">
+                <div className="formSelect">
+                  <TextField
+                    required
+                    id="businessUnit"
+                    name="businessUnit"
+                    variant="outlined"
+                    className="formSelector"
+                    label="Business unit"
+                    value={formData.businessUnit}
+                    onChange={handleChange}
+                  />
+                </div>
+                {/* make this a drop-down based on users with line manager permission */}
+                <div className="formSelect">
+                  <TextField
+                    name="userLineManager"
+                    className="formSelector"
+                    id="userLineManager"
+                    select
+                    label="Manager"
+                    defaultValue=""
+                    variant="outlined"
+                    onChange={handleChange}
+                  >
+                    {fullUsers.map((user) => {
+                      const userName = `${user.name.first} ${user.name.last}`;
+                      if (user.isLineManager) {
+                        return (
+                          <MenuItem
+                            className="cardFormValues"
+                            key={user._id}
+                            value={user._id}
+                          >
+                            {userName}
+                          </MenuItem>
+                        );
+                      }
+
+                      return null;
+                    })}
+                  </TextField>
+                </div>
               </div>
-              {/* make this a drop-down based on users with line manager permission */}
-              <div className="formSelect">
-                <TextField
-                  id="lineManager"
-                  name="lineManager"
-                  variant="outlined"
-                  className="formSelector"
-                  label="Manager"
-                  defaultValue=""
-                  select
-                  value={formData.userLineManager}
+              <div className="formRow">
+                <div className="formSelect">
+                  <TextField
+                    required
+                    className="formSelector"
+                    id="userRole"
+                    name="userRole"
+                    variant="outlined"
+                    label="User role"
+                    defaultValue=""
+                    select
+                    value={formData.userRole}
+                    onChange={handleChange}
+                  >
+                    {userRoles.map((option) => (
+                      <MenuItem
+                        className="cardFormValues"
+                        key={option.value}
+                        value={option.value}
+                      >
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </div>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Line manager"
+                  id="isLineManager"
+                  name="isLineManager"
+                  checked={formData.isLineManager}
                   onChange={handleChange}
                 />
               </div>
             </div>
-            <div className="formRow">
-              <div className="formSelect">
-                <TextField
-                  required
-                  className="formSelector"
-                  id="userRole"
-                  name="userRole"
-                  variant="outlined"
-                  label="User role"
-                  defaultValue=""
-                  select
-                  value={formData.userRole}
-                  onChange={handleChange}
-                >
-                  {userRoles.map((option) => (
-                    <MenuItem
-                      className="cardFormValues"
-                      key={option.value}
-                      value={option.value}
-                    >
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Line manager"
-                id="isLineManager"
-                name="isLineManager"
-                checked={formData.isLineManager}
-                onChange={handleChange}
-              />
+            <div className="formButton">
+              <Button type="submit" variant="contained" endIcon={<Send />}>
+                Add user
+              </Button>
             </div>
-          </div>
-          <div className="formButton">
-            <Button type="submit" variant="contained" endIcon={<Send />}>
-              Add user
-            </Button>
-          </div>
-        </form>
-        {/* Display the success message */}
-        {successMessage && (
-          <div className="successMessage">{successMessage}</div>
-        )}
-      </div>
+          </form>
+          {/* Display the success message */}
+          {successMessage && (
+            <div className="successMessage">{successMessage}</div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
